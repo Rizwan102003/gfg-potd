1962c74f92b7e3ec955a875208ad2321fcadbb22
Rizwan <skmdrizwan2003@gmail.com>
2024-03-31 21:57:33 +0530

    31

---
+++ 2024/March/31.cpp
@@ -0,0 +1,140 @@
+/*
+Closest Neighbour in BST
+
+PROBLEM STATEMENT :
+Given the root of a binary search tree and a number n, find the greatest number in the binary search tree that is less than or equal to n. 
+
+Your task :
+You don't need to read input or print anything. Your task is to complete the function findMaxForN() which takes the root of the BST, and the integer n as input parameters and returns the greatest element less than or equal to n in the given BST, Return -1 if no such element exists.
+
+Expected Time Complexity: O(Height of the BST)
+Expected Auxiliary Space: O(Height of the BST).
+
+Constraints:
+1 <= n <= 10^3
+1 <= elements of the BST <= 10^3
+All nodes are unique in the BST
+
+CODE :
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int key;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->key = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    int findMaxForN(Node* root, int n) {
+        if (!root)
+            return -1;
+
+        if (root->key <= n)
+            return max(root->key, findMaxForN(root->right, n));
+
+        return findMaxForN(root->left, n);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s;
+        getline(cin >> ws, s);
+        int x;
+        cin >> x;
+
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.findMaxForN(root, x) << endl;
+    }
+    return 1;
+}
+
+// } Driver Code Ends
